d8640c36f3324c977dfd0ff89e1e4af63ca5ba90
+ <+>
2022-08-24 00:16:38 +0300

    commit tree 5e1b0320fff77ae9050f0832b689fe5dec7824b6

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+const destructivelyAppendCat=function(name){
+    cats.push(name);
+    return cats
+}
+
+const destructivelyPrependCat=function(key){
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+    cats.unshift(key)
+    return cats
+}
+
+const destructivelyRemoveLastCat=function(value){
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+    cats.pop(value)
+}
+const destructivelyRemoveFirstCat=function(some){
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+    cats.shift(some)
+}
+const appendCat=function(min){
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+    const newcats=[...cats.slice(0),`${min}`]
+    return newcats
+}
+const prependCat=function(another){
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+    const othercats=[`${another}`,...cats.slice(0)]
+    return othercats
+}
+const removeLastCat=function(){
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+    const pets=cats.slice(0,2)
+    return pets
+}
+const removeFirstCat=function(){
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+    const felines=cats.slice(-2)
+    return felines
+}
